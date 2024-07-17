d5ead80718cc6a3ae6eb1d63e5ec0ab16985431d
AhmedFayed352 <fayedahmed352@gmail.com>
2024-07-17 22:05:10 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { MychartsComponent } from './components/mycharts/mycharts.component';
+import { SearchPipe } from './Pipes/search.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    MychartsComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/mycharts/mycharts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mycharts',
+  templateUrl: './mycharts.component.html',
+  styleUrls: ['./mycharts.component.css']
+})
+export class MychartsComponent {
+
+}
